e15604e48b49803995bb7efd82eb4bebea1f0bd1
Sandhya <kinnerasadnhya22@gmail.com>
2022-02-22 19:54:52 +0530

    English
    English

---
+++ Sample english ebook - Storyline output/story_content/user.js
@@ -0,0 +1,277 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6EnNcURBkcI":
+        Script1();
+        break;
+      case "6Ait9XOtbwb":
+        Script2();
+        break;
+      case "6UYheR4nbx7":
+        Script3();
+        break;
+      case "5rIcI3Zxr2P":
+        Script4();
+        break;
+      case "6dQLjDQDojC":
+        Script5();
+        break;
+      case "68oTix2sDIS":
+        Script6();
+        break;
+      case "5pZPXcH5Ba3":
+        Script7();
+        break;
+      case "5hXrOx1MsNK":
+        Script8();
+        break;
+      case "67Aae11sCgI":
+        Script9();
+        break;
+      case "6CVyMVHEEXq":
+        Script10();
+        break;
+      case "6Za5XzjRx6B":
+        Script11();
+        break;
+      case "5XHRgfSGxxI":
+        Script12();
+        break;
+      case "5fpTo3tHE43":
+        Script13();
+        break;
+      case "63Ay6Vr108e":
+        Script14();
+        break;
+      case "5sXvMaMSU6M":
+        Script15();
+        break;
+      case "6dyZr80tQvv":
+        Script16();
+        break;
+      case "6RjxMsbyivK":
+        Script17();
+        break;
+      case "5avljWcdSMG":
+        Script18();
+        break;
+      case "6XC03FpNYMk":
+        Script19();
+        break;
+      case "61Qo9tAgKJ9":
+        Script20();
+        break;
+      case "5kxdr66gekN":
+        Script21();
+        break;
+      case "6qlhgKqSdUe":
+        Script22();
+        break;
+      case "6pPgzHIZlJw":
+        Script23();
+        break;
+      case "6ayfT1pvX1d":
+        Script24();
+        break;
+      case "5g1QVsENsUz":
+        Script25();
+        break;
+      case "66onzxsxKhj":
+        Script26();
+        break;
+      case "6hP2PDSL9BR":
+        Script27();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+var addq = player.GetVar("namefillx") + "q";
+player.SetVar("namefillx", addq);
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+var addw = player.GetVar("namefillx") + "w";
+player.SetVar("namefillx", addw);
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+var adde = player.GetVar("namefillx") + "e";
+player.SetVar("namefillx", adde);
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+var addr = player.GetVar("namefillx") + "r";
+player.SetVar("namefillx", addr);
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+var addt = player.GetVar("namefillx") + "t";
+player.SetVar("namefillx", addt);
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+var addy = player.GetVar("namefillx") + "y";
+player.SetVar("namefillx", addy);
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+var addu = player.GetVar("namefillx") + "u";
+player.SetVar("namefillx", addu);
+}
+
+function Script8()
+{
+  var player = GetPlayer();
+var addi = player.GetVar("namefillx") + "i";
+player.SetVar("namefillx", addi);
+}
+
+function Script9()
+{
+  var player = GetPlayer();
+var addo = player.GetVar("namefillx") + "o";
+player.SetVar("namefillx", addo);
+}
+
+function Script10()
+{
+  var player = GetPlayer();
+var addp = player.GetVar("namefillx") + "p";
+player.SetVar("namefillx", addp);
+}
+
+function Script11()
+{
+  var player = GetPlayer();
+var adda = player.GetVar("namefillx") + "a";
+player.SetVar("namefillx", adda);
+}
+
+function Script12()
+{
+  var player = GetPlayer();
+var adds = player.GetVar("namefillx") + "s";
+player.SetVar("namefillx", adds);
+}
+
+function Script13()
+{
+  var player = GetPlayer();
+var addd = player.GetVar("namefillx") + "d";
+player.SetVar("namefillx", addd);
+}
+
+function Script14()
+{
+  var player = GetPlayer();
+var addf = player.GetVar("namefillx") + "f";
+player.SetVar("namefillx", addf);
+}
+
+function Script15()
+{
+  var player = GetPlayer();
+var addg = player.GetVar("namefillx") + "g";
+player.SetVar("namefillx", addg);
+}
+
+function Script16()
+{
+  var player = GetPlayer();
+var addh = player.GetVar("namefillx") + "h";
+player.SetVar("namefillx", addh);
+}
+
+function Script17()
+{
+  var player = GetPlayer();
+var addj = player.GetVar("namefillx") + "j";
+player.SetVar("namefillx", addj);
+}
+
+function Script18()
+{
+  var player = GetPlayer();
+var addk = player.GetVar("namefillx") + "k";
+player.SetVar("namefillx", addk);
+}
+
+function Script19()
+{
+  var player = GetPlayer();
+var addl = player.GetVar("namefillx") + "l";
+player.SetVar("namefillx", addl);
+}
+
+function Script20()
+{
+  var player = GetPlayer();
+var addz = player.GetVar("namefillx") + "z";
+player.SetVar("namefillx", addz);
+}
+
+function Script21()
+{
+  var player = GetPlayer();
+var addx = player.GetVar("namefillx") + "x";
+player.SetVar("namefillx", addx);
+}
+
+function Script22()
+{
+  var player = GetPlayer();
+var addc = player.GetVar("namefillx") + "c";
+player.SetVar("namefillx", addc);
+}
+
+function Script23()
+{
+  var player = GetPlayer();
+var addv = player.GetVar("namefillx") + "v";
+player.SetVar("namefillx", addv);
+}
+
+function Script24()
+{
+  var player = GetPlayer();
+var addb = player.GetVar("namefillx") + "b";
+player.SetVar("namefillx", addb);
+}
+
+function Script25()
+{
+  var player = GetPlayer();
+var addn = player.GetVar("namefillx") + "n";
+player.SetVar("namefillx", addn);
+}
+
+function Script26()
+{
+  var player = GetPlayer();
+var addm = player.GetVar("namefillx") + "m";
+player.SetVar("namefillx", addm);
+}
+
+function Script27()
+{
+  var player = GetPlayer();
+var addSpace = player.GetVar("namefillx") + " ";
+player.SetVar("namefillx", addSpace);
+}
+
